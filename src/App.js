e08f8a2afd4c816b4718bc6a28049cbf149c362c
kousigan <kousiunleashed@gmail.com>
2021-04-07 19:30:17 +0530

    updating error message

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import TaskPanel from "./panel/TaskPanel";
 import { days, months } from "./panel/names";
 import { db } from "./db/config";
 import { nanoid } from "nanoid";
-import box from './img/box.svg';
-import delivery from './img/delivery.svg';
+import box from "./img/box.svg";
+import delivery from "./img/delivery.svg";
 import "./style.css";
 
 class App extends React.Component {
@@ -34,11 +34,9 @@ class App extends React.Component {
       theList: [],
       loadingData: true,
       user: null,
-      userLoaded: false
+      userLoaded: false,
+      showerror: false
     };
-
-
-
   }
 
   handleDate = e => {
@@ -53,7 +51,7 @@ class App extends React.Component {
         prevState.fdate = temp;
       }
       console.log(this.state.user);
-      if(this.state.user!==null){
+      if (this.state.user !== null) {
         console.log(this.state.user);
         this.getData(this.state.user);
       }
@@ -65,13 +63,12 @@ class App extends React.Component {
     var temp = nanoid(4) + "-" + uid + "-" + nanoid(4) + "-" + uid + 1;
     return temp;
   };
-  getData = (user) => {
-     
-     var temp = user;
-    
-    db.collection("todo-"+temp)
-      .where("date.day", "==", parseInt(this.state.fdate.day)) 
-      .where("date.month", "==", parseInt(this.state.fdate.month)) 
+  getData = user => {
+    var temp = user;
+
+    db.collection("todo-" + temp)
+      .where("date.day", "==", parseInt(this.state.fdate.day))
+      .where("date.month", "==", parseInt(this.state.fdate.month))
       .onSnapshot(snapshot => {
         temp = snapshot.docs.map(doc => ({
           id: doc.id,
@@ -89,30 +86,43 @@ class App extends React.Component {
     this.setState({ loadingData: false });
     return this.state.theList;
   };
-  checkUser = (e) => {
-
+  checkUser = e => {
+    if(e.target.value.length>0){
+      this.setState({ showerror: false });
+    }
     if (e.key == "Enter") {
       var temp = e.target.value.toLowerCase();
-      if(temp=="kousi"||"preeti")
-      this.setState({ user : temp });
-      this.getData(temp);
-      this.setState({ userLoaded: true });
+      if (temp == "kousi" || temp == "preeti") {
+        this.setState({ user: temp });
+        this.getData(temp);
+        this.setState({ userLoaded: true });
+      } else {
+        this.setState({ showerror: true });
+      }
     }
     // this.setState({ userLoaded: true });
-  }
+  };
+  errorMessage = () => {
+    return <div className="section">nope.. try agin!!</div>;
+  };
   getUserId = () => {
     return (
-
       <div className="overlay-container getModal">
         <div className="card getusername">
           <h3 className="section">Who's this?</h3>
           <label htmlFor="username">Username</label>
-          <input type="text" id="Username" placeholder="Username" onKeyDown={this.checkUser} />
+          <input
+            type="text"
+            id="Username"
+            placeholder="Username"
+            onKeyDown={this.checkUser}
+          />
+          {this.state.showerror ? this.errorMessage() : ""}
         </div>
       </div>
-    )
-  }
-  
+    );
+  };
+
   makeCards = data => {
     // console.log("makeCards", data);
     if (data.length > 0) {
@@ -121,37 +131,35 @@ class App extends React.Component {
           {data.map(task => (
             <TaskCard
               key={task.id}
-              user ={this.state.user}
+              user={this.state.user}
               id={task.id}
               name={task.name}
-                status={task.status}
+              status={task.status}
             />
           ))}
         </div>
       );
-    }
-    else {
+    } else {
       return (
         <figure>
           <img src={box} alt="Empty list" />
           <figcaption>There are no items for the day.</figcaption>
         </figure>
-      )
+      );
     }
   };
 
-  loadingData =()=>{
+  loadingData = () => {
     return (
       <figure>
         <img src={delivery} alt="Loading data" />
         <figcaption>Items are getting loaded.</figcaption>
       </figure>
-    )
-  
-  }
+    );
+  };
 
   submitTask = e => {
-    if (e.key == "Enter"&&this.state.user!==null) {
+    if (e.key == "Enter" && this.state.user !== null) {
       const { todo } = this.state;
       todo.name = e.target.value;
       todo.taskId = this.uidgen();
@@ -160,7 +168,7 @@ class App extends React.Component {
       todo.date.month = parseInt(this.state.fdate.month);
       todo.date.year = parseInt(this.state.fdate.year);
       console.log(todo);
-      db.collection("todo-"+this.state.user)
+      db.collection("todo-" + this.state.user)
         .add(todo)
         .then(() => {
           console.log("success");
@@ -176,21 +184,19 @@ class App extends React.Component {
 
     return (
       <div className="container">
-
         <div className="row">
           <div className="col-sm monthly-calendar ">
-            <MonthlyCalendar fixDate={this.handleDate} /> 
+            <MonthlyCalendar fixDate={this.handleDate} />
           </div>
           <div className=" .taskpanel col-lg-offset-1 col-lg-8">
-           <h4> welcome  {this.state.user==null ? '':this.state.user}</h4>
+            <h4> welcome {this.state.user == null ? "" : this.state.user}</h4>
             <TaskPanel today={newDay_} pushData={this.submitTask} user={user} />
-            {this.state.userLoaded ? '' : this.getUserId()}
+            {this.state.userLoaded ? "" : this.getUserId()}
             <h3>Tasks</h3>
             {this.state.loadingData
               ? this.loadingData()
               : this.makeCards(this.state.theList)}
           </div>
-
         </div>
       </div>
     );
@@ -200,37 +206,40 @@ class App extends React.Component {
 
 export default App;
 
- 
 class TaskCard extends React.Component {
-constructor(props){
-  super(props);
-}
-
-pushUpdate=(e)=>{
-var docId=e.target.getAttribute('data-key');
-var userId=e.target.getAttribute('data-user');
-console.log(docId,userId);
-var taskRef = db.collection('todo-'+userId).doc(docId)
-taskRef.get().then((snapshot)=>{
-  if(!snapshot.data().status){
-    return taskRef.update({status: true})
-  } else{
-    return taskRef.update({status: false})
+  constructor(props) {
+    super(props);
   }
-})
 
- }
-  render(){
-    console.log(this.props.user)
-   return (
-    
-    <div className="card"  onClick={this.pushUpdate} data-status={this.props.status}>
-      <div className="section" data-user={this.props.user} data-key={this.props.id}>
-      <label   className="status-icon" ></label>
-        {" "}
-        {this.props.name}{" "}
+  pushUpdate = e => {
+    var docId = e.target.getAttribute("data-key");
+    var userId = e.target.getAttribute("data-user");
+    console.log(docId, userId);
+    var taskRef = db.collection("todo-" + userId).doc(docId);
+    taskRef.get().then(snapshot => {
+      if (!snapshot.data().status) {
+        return taskRef.update({ status: true });
+      } else {
+        return taskRef.update({ status: false });
+      }
+    });
+  };
+  render() {
+    console.log(this.props.user);
+    return (
+      <div
+        className="card"
+        onClick={this.pushUpdate}
+        data-status={this.props.status}
+      >
+        <div
+          className="section"
+          data-user={this.props.user}
+          data-key={this.props.id}
+        >
+          <label className="status-icon" /> {this.props.name}{" "}
+        </div>
       </div>
-    </div>
-  );}
-};
-
+    );
+  }
+}
